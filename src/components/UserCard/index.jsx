77e28f99ccd862b045e1bfda63039979ded25de4
ArtyomHloba <hlobartyom@gmail.com>
2024-07-08 11:55:27 +0300

    Add heart & logic

--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import styles from './UserCard.module.css'
 
 
@@ -6,34 +7,62 @@ function UserCard(props) {
     userName, userSurname, userAge, isMale, userPhoto, userTG, userGitHub, userFollowers
   } = props
 
-  const isMaleColor ={
-    color: isMale ? '#33A8D9' : 'pink',
-  }
+  const [liked, setLiked] = useState(false);
+  const [followers, setFollowers] = useState(userFollowers);
 
-  const ageColor ={
+  const handleLike = () => {
+    if (liked) {
+      setFollowers(followers - 1);
+    } else {
+      setFollowers(followers + 1);
+    }
+    setLiked(!liked);
+  };
+
+  const isMaleColor = {
+    color: isMale ? '#33A8D9' : 'pink',
+  };
+
+  const ageColor = {
     color: userAge >= 18 ? 'green' : 'red',
-}
+  };
+
+  const likeButtonColor = {
+    color: liked ? 'red' : 'grey',
+  };
 
   return (
     <article className={styles.userCard}>
       <img className={styles.userPhoto} src={userPhoto} alt="UserPhoto" />
-      <h2 className={styles.userNameStyle} style={isMaleColor}>{userName} {userSurname}</h2>
+      <h2 className={styles.userNameStyle} style={isMaleColor}>
+        {userName} {userSurname}
+      </h2>
       <p className={styles.userPosition}>FULL STACK DEVELOPER</p>
-      <p className={styles.ageStyle}>Age: <span style={ageColor}>{userAge}</span></p>
+      <p className={styles.ageStyle}>
+        Age: <span style={ageColor}>{userAge}</span>
+      </p>
 
       <div className={styles.linksCont}>
-          <a className={styles.linkTg} href={userTG}><i className="fa-brands fa-telegram"></i></a>
-          <a className={styles.linkGit} href={userGitHub}><i className="fa-brands fa-github"></i></a>
+        <a className={styles.linkTg} href={userTG}>
+          <i className="fa-brands fa-telegram"></i>
+        </a>
+        <a className={styles.linkGit} href={userGitHub}>
+          <i className="fa-brands fa-github"></i>
+        </a>
       </div>
-      
+
       <div className={styles.likeCont}>
-        <p>{userFollowers}</p>
-        <button className={styles.heart}><i className="fa-regular fa-heart"></i></button>
+        <p>{followers}</p>
+        <button
+          className={styles.heart}
+          style={likeButtonColor}
+          onClick={handleLike}
+        >
+          <i class="fa-solid fa-heart"></i>
+        </button>
       </div>
-
-
     </article>
-  )
+  );
 }
 
 export default UserCard;
