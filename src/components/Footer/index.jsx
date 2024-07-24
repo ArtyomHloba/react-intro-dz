6e94a266e2ed864e96be1b45144e634de35f9207
ArtyomHloba <hlobartyom@gmail.com>
2024-07-24 12:42:47 +0300

    Add footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,18 @@
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 import styles from "./Footer.module.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footerPage}>
+      <a href="#" className={styles.instagramLink}>
+        <FaInstagram /> Instagram
+      </a>
+      <a href="#" className={styles.facebookLink}>
+        <FaFacebook /> Facebook
+      </a>
+    </footer>
+  );
 }
 
 export default Footer;
